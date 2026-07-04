b038315cd0cf1e12befa7fe5742a24e7531130e7
vburnin8tor <32801584+vburnin8tor@users.noreply.github.com>
2026-07-04 12:37:23 -0400

    Update version to 2.3-approved

--- torn-usd-converter.user.js
+++ torn-usd-converter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn USD Converter
 // @author       shaul [3908280]
-// @version      2.3
+// @version      2.3-approved
 // @description  Convert Torn cash displays to USD equivalents
 // @match        https://www.torn.com/*
 // @grant        none
